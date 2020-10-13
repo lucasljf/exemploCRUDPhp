fe5e802184d09438de2f0749563baf4f6c791733
Lucas Faria <lucasljf@gmail.com>
2020-10-13 09:32:08 -0300

    Add arquivos Docker

--- db/conexao.php
+++ db/conexao.php
@@ -1,9 +1,9 @@
 <?php
     class Conexao {
-        private $host = 'localhost';
+        private $host = 'db'; //para xampp ou instalação separada, substituir por 'localhost'
         private $db = 'projetoAlunos';
         private $usuario = 'root';
-        private $senha = '123456789';
+        private $senha = '123456789'; //altere a senha de acordo com seu ambiente de desenvolvimento
 
         public function conectar() {
             try {
